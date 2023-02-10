92f7fd843a063ff20a4def6317886d2c22883578
Zeeshan Ahmad <zeeshan.za.ahmad@gmail.com>
2023-02-11 02:13:05 +0500

    User can join group

--- app/controllers/group_users_controller.rb
+++ app/controllers/group_users_controller.rb
@@ -4,4 +4,25 @@ class GroupUsersController < ApplicationController
     def index
         @group_users = current_user.groups.all
     end
+
+    # POST /group_users or /group_user.json
+    def create
+        @group_user = GroupUser.new(group_users_params.merge(user_id: current_user.id))
+
+        respond_to do |format|
+            if @group_user.save
+                format.html { redirect_to group_url(@group_user), notice: "Joined successfully." }
+                format.json { render :show, status: :created, location: @group_user }
+            else
+                format.html { render :new, status: :unprocessable_entity }
+                format.json { render json: @group_user.errors, status: :unprocessable_entity }
+            end
+        end
+    end
+
+    private
+
+    def group_users_params
+      params.fetch(:group_users, {}).permit(:group_id)
+    end
 end
