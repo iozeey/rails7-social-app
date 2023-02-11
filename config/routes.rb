9fe12c7044eefa5baf5d2af652acef3274ae4451
Zeeshan Ahmad <zeeshan.za.ahmad@gmail.com>
2023-02-12 04:00:32 +0500

    Update root to groups

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root to: "home#index" 
+  root to: "groups#index" 
 end
