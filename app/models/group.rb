e9ca63872acac240a48eca92be791981dc0a1ee4
Zeeshan Ahmad <zeeshan.za.ahmad@gmail.com>
2023-02-12 03:57:51 +0500

    Add validations and update last activity in group and post

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,10 @@ class Group < ApplicationRecord
     has_many :posts
     has_many :group_users
     has_many :groups, through: :group_users
+
+    before_save :set_last_activity
+
+    def set_last_activity
+        self.last_activity = Time.now
+    end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,11 @@
 class Post < ApplicationRecord
     has_many :comments
+    validates_presence_of :title
+    validates_presence_of :content
+    
+    before_save :set_last_activity
+
+    def set_last_activity
+        self.last_activity = Time.now
+    end
 end
