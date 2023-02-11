15b895bda264db7573b3d871a41249cfe2342fa6
Zeeshan Ahmad <zeeshan.za.ahmad@gmail.com>
2023-02-12 04:01:05 +0500

    Test with stimulus

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,14 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  connect() {
-    this.element.textContent = "Hello World!"
+
+  static targets = ["name"]
+
+  greet() {
+    console.log("Hello, Stimulus!", this.name)
+  }
+
+  get name() {
+    return this.nameTarget.value
   }
 }
